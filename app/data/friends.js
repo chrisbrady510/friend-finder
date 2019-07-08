63a66c8d69a2ba7d40f3124f30a11251325ace74
chrisbrady510 <chrismbrady@gmail.com>
2019-07-08 07:06:43 -0700

    Friend Finder complete

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+// Array that holds data objects, a list of all friends and applicable data
+
+// Might need to change the datatype of answers to string
+module.exports = [
+	{
+		name: "Woody",
+		photo: ("../images/woody.png"),
+		answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	},
+	{
+		name: "Buzz Lightyear",
+		photo: "../images/buzz.png",
+		answers: [1, 4, 1, 5, 5, 2, 3, 5, 3, 1]
+	},
+	{
+		name: "Bo Peep",
+		photo: "../images/bopeep.png",
+		answers: [2, 4, 1, 3, 1, 4, 2, 5, 1, 1]
+	},
+	{
+		name: "Forky",
+		photo: "../images/forky.png",
+		answers: [1, 1, 5, 1, 5, 2, 5, 5, 1, 1]
+	},
+	{
+		name: "Gabby Gabby",
+		photo: "../images/gabby.png",
+		answers: [3, 4, 1, 4, 2, 3, 2, 4, 3, 1]
+	},
+	
+	{
+		name: "Duke Caboom",
+		photo: "../images/duke.png",
+		answers: [1, 3, 1, 1, 3, 5, 3, 5, 5, 5]
+	},
+	{
+		name: "Bunny and Ducky",
+		photo: "../images/bunny.png",
+		answers: [5, 1, 1, 1, 5, 1, 5, 5, 1, 1]
+	},
+	
+	
+	
+
+];
+
+// module.exports.push2arr = function(data) {
+// 	module.exports.allFriends.push(data);
+// }
